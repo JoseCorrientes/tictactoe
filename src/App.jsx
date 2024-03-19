0997da6dcd9c145bce76e35c1f976647cf2b4fdd
Jose Ernesto Garcia <josernestogarcia609@gmail.com>
2024-03-19 13:56:07 -0300

    20230319-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Board from './components/Board/Board'
+import Fondo from "../src/assets/14063419_5431981.jpg"
 
 function App() {
  
@@ -8,7 +9,7 @@ function App() {
   return (
     <div
         className='flex flex-row w-auto h-screen bg-cover'
-        style={{backgroundImage: "url('../src/assets/14063419_5431981.jpg')"}}
+        style={{backgroundImage: `url(${Fondo})`}}
         // style={{backgroundImage: "url('/src/assets/14063419_5431981.jpg')"}}
     >
     <BrowserRouter>
