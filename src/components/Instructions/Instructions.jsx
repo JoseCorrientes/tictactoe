0136176ae67472bde3ba4f49c983c18ecb7397ec
Jose Ernesto Garcia <josernestogarcia609@gmail.com>
2024-03-22 09:58:02 -0300

    202040322-3

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -23,7 +23,7 @@ function Instructions({handleSubmit, language, messages}) {
     className="flex flex-row justify-center items-center fixed inset-0 w-screen h-screen bg-black bg-opacity-70  "
 >
             <div
-                className="flex flex-col  items-center w-full sm:w-auto px-0 sm:px-10 mt-2 sm:mt-0 h-auto m-1 sm:m-0 sm:h-auto  border-4 rounded-3xl bg-black bg-opacity-100 shadow-white shadow-lg  ">
+                className="flex flex-col  items-center w-full sm:w-auto px-0 sm:px-10  h-auto m-1 sm:m-0 sm:h-auto  border-4 rounded-3xl bg-black bg-opacity-100 shadow-white shadow-lg  ">
                 
                     <p
                         className="font-fontdinerSwanky text-2xl sm:text-3xl text-gray-400 sm:mt-10 sm:mb-5 mt-5"
